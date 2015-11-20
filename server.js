1de8e6f18b21d638191984aed1b2ca09cb3a9005
Tyler Hoffman <tyler.c.hoffman@gmail.com>
2015-11-20 10:33:00 -0500

    Add 404

--- server.js
+++ server.js
@@ -6,14 +6,14 @@ var port = 8080;
 app.set('view engine', 'ejs');
 
 app.get('/', function(req, res) {
-  res.render('pages/index.ejs');
+  res.render('pages/index');
 });
 
 app.get('/forums/thread', function(req, res) {
   var user = require('./app/models/users/user');
   var post = require('./app/models/forums/post');
   var thread = require('./app/models/forums/thread');
-  res.render('pages/forum-thread.ejs', {
+  res.render('pages/forum-thread', {
     User: user,
     Post: post,
     Thread: thread
@@ -22,6 +22,11 @@ app.get('/forums/thread', function(req, res) {
 
 app.use(morgan('dev'));
 app.use(express.static('./public'));
+
+app.use(function(req, res, next) {
+  res.status(404);
+  res.render('pages/404', {url: req.url});
+});
 
 app.listen(port);
 console.log('listening on port ' + port);
