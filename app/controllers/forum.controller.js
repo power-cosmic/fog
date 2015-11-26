c09dd9d2e72dfb3e4d833c70a43b0adfd3c12e8a
Tyler Hoffman <tyler.c.hoffman@gmail.com>
2015-11-26 14:59:36 -0500

    Move dummy data generation to server (for now)

--- app/controllers/forum.controller.js
+++ app/controllers/forum.controller.js
@@ -2,14 +2,10 @@ var Thread = require('../models/forums/thread'),
     Post = require('../models/forums/post');
 
 exports.readThread = function(req, res) {
-  var user = require('../models/users/user'),
-      post = require('../models/forums/post'),
-      thread = require('../models/forums/thread');
-      
+
+
   res.render('forums/pages/forum-thread', {
-    User: user,
-    Post: post,
-    Thread: thread
+    thread: req.thread
   });
 };
 
@@ -20,5 +16,23 @@ exports.postById = function(req, res, next, id) {
 
 exports.threadById = function(req, res, next, id) {
   console.log('thread id: ' + id);
+
+  var User = require('../models/users/user'),
+      Post = require('../models/forums/post'),
+      Thread = require('../models/forums/thread');
+
+  var tyler = new User('thoffma7');
+  var thread = new Thread(tyler, 'hello world', 'hey folks');
+  var firstPost = new Post(tyler, 'oh, hi');
+  thread.addReply(firstPost);
+  var reply = new Post(tyler, 'hello to you too');
+  firstPost.addReply(reply);
+  reply.addReply(new Post(tyler, 'nested hi'));
+  reply.replies[0].addReply(new Post(tyler, 'this is soo nested'));
+  reply.addReply(new Post(tyler, 'another nested hi'));
+  thread.addReply(new Post(tyler, 'hello!!!!!!!!'));
+
+  req.thread = thread;
+
   next();
 };
