48c95cff083b0e627a43ddd29d8451a827dc0e88
Tyler Hoffman <tyler.c.hoffman@gmail.com>
2015-12-02 00:05:30 -0500

    Update mongo part of dev.controller a bit

--- app/controllers/dev.controller.js
+++ app/controllers/dev.controller.js
@@ -24,6 +24,20 @@ exports.acceptGame = function(req, res) {
       inputPath = './uploads/games/pending/' + game.files.compressed,
       outputPath = './uploads/games/published/' + sanitizedTitle;
 
+      // insert into real db collection
+      MongoClient.connect(config.db, function(err, db) {
+        db.collection('games').insert({
+          title: game.title,
+          description: game.description,
+          url: outputPath,
+          config: game.config
+        }, function (err, game) {
+            req.game = game;
+            db.close();
+            next();
+        });
+      });
+
       fs.createReadStream(inputPath).pipe(unzip.Extract({ path: outputPath }));
   res.json({
     status: 'success'
@@ -42,10 +56,17 @@ exports.addNew = function(req, res) {
     if (err) {
       console.log('error: ' + err);
     }
-    var newGame = new Game(body.gameTitle, 'thoffma7.dev', {
-      compressed: fileName
-    }, body.description, body.instructions);
-    db.collection('pendingGames').insertOne(newGame);
+    // var newGame = new Game(body.gameTitle, 'thoffma7.dev', {
+    //   compressed: fileName
+    // }, body.description, body.instructions);
+    db.collection('pendingGames').insertOne({
+      title: body.title,
+      description: body.description,
+      config: body.config,
+      files: {
+        compressed: fileName
+      }
+    });
     db.close();
     res.redirect('/dev/games/pending/' + fileName);
     // res.json({
