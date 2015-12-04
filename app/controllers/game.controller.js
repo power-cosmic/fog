3aaacf7a268a5cca329a6a658d6a357ac337c051
Eddie Gurnee <pegurnee@gmail.com>
2015-12-04 18:09:14 -0500

    game fix

--- app/controllers/game.controller.js
+++ app/controllers/game.controller.js
@@ -14,9 +14,8 @@ exports.play = function(req, res) {
     scripts: game.config.scripts || [],
     styles: game.config.styles || [],
     startingPoint: game.config.startingPoint,
-    gamePath: req.game.gamePath,
-    cookie: req.cookies,
-    gamePath: '/game-files/' + req.game.gamePath
+    gamePath: '/game-files/' + req.game.gamePath,
+    cookie: req.cookies
   });
 };
 
