83e6253c0d95fd91801de74ac3b6a47df70e632d
Tyler Hoffman <tyler.c.hoffman@gmail.com>
2015-12-03 23:48:26 -0500

    Finalize downloads and a bit of cleanup

--- app/controllers/game.controller.js
+++ app/controllers/game.controller.js
@@ -7,8 +7,7 @@ var config = require('../../config/config'),
 
 exports.play = function(req, res) {
   var game = req.game;
-  console.log('PLAY')
-  console.log('play', game);
+
   res.render('games/pages/game', {
     title: game.title,
     scripts: game.config.scripts || [],
@@ -23,28 +22,6 @@ var extract = function(game) {
       outputPath = './uploads/games/published/' + game.developer + '/',
       originalDirectoryName = game.originalFilename.replace(/\..*/, ''),
       configPath = (outputPath + '/' + originalDirectoryName + '/fog.json');
-          //.replace(/ /, '\\ ');
-
-  // fs.mkdirSync(outputPath);
-  // fs.createReadStream(inputPath)
-  // .pipe(unzip.Parse())
-  // .on('entry', function (entry) {
-  //   var fileName = entry.path;
-  //   var type = entry.type; // 'Directory' or 'File'
-  //   var size = entry.size;
-  //   console.log(fileName);
-  //   if (fileName !== ".git") {
-  //     console.log(fileName);
-  //     entry.pipe(fs.createWriteStream(outputPath));
-  //   } else {
-  //     entry.autodrain();
-  //   }
-  // });
-
-  console.log('EXTRACTING');
-  console.log('in:', inputPath);
-  console.log('out', outputPath)
-  console.log('!!! ' + configPath);
 
   var unzipper = new DecompressZip(inputPath);
 
@@ -54,7 +31,6 @@ var extract = function(game) {
 
   unzipper.on('extract', function (log) {
       console.log('Finished extracting');
-
   });
 
   unzipper.extract({
@@ -63,38 +39,16 @@ var extract = function(game) {
           return file.type !== "SymbolicLink";
       }
   });
-  console.log('done');
 
-  // var unzipper = unzip.Extract({ path: outputPath });
-  //
-  // console.log('JAXING')
-  // unzipper.on('error', function(err) {
-  //   console.log('ERROR', err);
-  // });
-  // unzipper.on('close', function(fn) {
-  //   console.log('CLOSE', fn);
-  // });
-  //
-  //
-  // fs.createReadStream(inputPath).pipe(unzipper);
 };
 
 exports.accept = function(req, res) {
-  console.log("ACCEPT GAME");
   var game = req.game,
       inputPath = './uploads/games/pending/' + game.files.compressed,
       outputPath = './uploads/games/published/' + game.developer,
       originalDirectoryName = game.originalFilename.replace(/\..*/, ''),
       configPath = (outputPath + '/' + originalDirectoryName + '/fog.json');
-          //.replace(/ /, '\\ ');
 
-  console.log('ORIG', originalDirectoryName)
-  console.log('in:', inputPath);
-  console.log('out', outputPath)
-  console.log('!!! ' + configPath);
-
-
-  console.log('configPath', configPath);
   var gameConfig = JSON.parse(fs.readFileSync(configPath));
 
   MongoClient.connect(config.db, function(err, db) {
@@ -133,21 +87,14 @@ exports.create = function(req, res) {
       filePath = '/' + file.path.replace(/(\.\.\/)*/, ''),
       fileName = file.path.replace(/(.*\/)*/, '');
 
-      //sanitizedTitle = sanitize(body.gameTitle.replace(/\..*/, ''));
-      //sanitizedFileName = sanitize(file.originalname);
-  console.log(body);
-
   MongoClient.connect(config.db, function(err, db) {
     if (err) {
       console.log('error: ' + err);
     }
-    // var newGame = new Game(body.gameTitle, 'thoffma7.dev', {
-    //   compressed: fileName
-    // }, body.description, body.instructions);
+
     db.collection('games').insertOne({
       title: body.gameTitle,
       description: body.description,
-      //sanitizedTitle: sanitizedTitle,
       developer: 'thoffman_dev',
       originalFilename: file.originalname,
       status: 'pending',
@@ -158,11 +105,7 @@ exports.create = function(req, res) {
     }, function(err, inserted) {
       inserted = inserted.ops[0];
       extract(inserted);
-      console.log('---')
-      console.log(err, inserted);
-      console.log('---')
       db.close();
-      console.log('CREATED')
       res.redirect('/game/pending/' + inserted._id);
     });
   });
--- public/helpers/games/gameKickerOff.js
+++ public/helpers/games/gameKickerOff.js
@@ -7,7 +7,6 @@ $(function() {
   var fn = gameDiv.getAttribute('data-fn'),
       path = gameDiv.getAttribute('data-path');
 
-  console.log(fn);
   // pass the function the game div and hand over control
   window[fn].call(window, gameDiv, path);
 });
