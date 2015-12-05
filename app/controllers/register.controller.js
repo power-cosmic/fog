a10be19eac91ec752df62749c20cdae6cfe5f381
Tyler Hoffman <tyler.c.hoffman@gmail.com>
2015-12-05 14:36:57 -0500

    Hashbrowns and salt

--- app/controllers/register.controller.js
+++ app/controllers/register.controller.js
@@ -1,7 +1,13 @@
 var User = require('../models/users/user'),
   MongoClient = require('mongodb').MongoClient,
   ObjectId = require('mongodb').ObjectID,
-  database = require('../../config/config').db;
+  crypto = require('crypto'),
+  database = require('../../config/config').db,
+  success = 0,
+  invalidUsername = 1,
+  invalidPassword = 2,
+  userExists = 3,
+  insertError = 4;
 
 exports.begin = function(req, res) {
   res.render('register/pages/register-home', {
@@ -9,22 +15,46 @@ exports.begin = function(req, res) {
   });
 };
 
+exports.hashAndSalt = function(rawPassword, salt) {
+  return crypto
+      .createHash('sha1')
+      .update(rawPassword + salt)
+      .digest('hex');
+};
+
 exports.login = function(req, res) {
+  exports.getUser(req.body.username, req.body.password, function(err, user) {
+    switch (err) {
+      case invalidUsername:
+        res.send('That username doesn\'t exist');
+        break;
+      case invalidPassword:
+        res.send('That\'s not your password!');
+        break;
+      case success:
+        req.session.user = user;
+        res.send('success');
+    }
+  });
+};
+
+exports.getUser = function(username, password, callback) {
   MongoClient.connect(database, function(err, db) {
     try {
-      db.collection('users').findOne(
-        {
-          $and: [
-            {"username": req.body.username},
-            {"password": req.body.password}
-          ]
+      db.collection('users').findOne({
+          username: username
         },
         function (err, doc) {
-          if (doc === null) {
-            res.send("username and password do not match");
+          if (!doc) {
+            callback(invalidUsername);
           } else {
-            req.session.user = User.fromMongo(doc);
-            res.send("success");
+
+            var hashed = exports.hashAndSalt(password, doc.salt);
+            if (hashed !== doc.password) {
+              callback(invalidPassword);
+            } else {
+              callback(success, doc);
+            }
           }
           db.close();
       });
@@ -33,6 +63,8 @@ exports.login = function(req, res) {
     }
   });
 };
+
+
 
 exports.auth = function(req, res) {
   // var lookup = login(req.cookies['username'], req.cookies['password']);
@@ -44,23 +76,45 @@ exports.auth = function(req, res) {
   // });
 };
 
-exports.create = function(req, res) {
+exports.findById = function(id, callback) {
+  MongoClient.connect(database, function(err, db) {
+    db.collection('users').findOne({_id: ObjectId(id)}, function(err, user) {
+      db.close();
+      callback(err, user);
+    });
+  });
+};
+
+exports.register = function(user, callback) {
+  user.salt = crypto.randomBytes(16);
+  user.password = exports.hashAndSalt(user.password, user.salt);
+
   MongoClient.connect(database, function(err, db) {
     try {
-      db.collection('users').insertOne(req.body,
+      db.collection('users').insertOne(user,
         function (err, result) {
+          db.close();
           if(result === null) {
-            res.send('failed to create new user!');
+            callback(insertError);
           } else {
             var user = new User.fromMongo(result.ops[0]);
-            req.session.user = user;
-            res.render('common/pages/index');
+            callback(success, user);
           }
-          db.close();
       });
     } catch (e) {
       console.log('Couldn\'t create user ' + userName);
 
+    }
+  });
+};
+
+exports.create = function(req, res) {
+  exports.register(req.body, function(err, user) {
+    if (err) {
+      res.send('failed to create new user!');
+    } else {
+      req.session.user = user;
+      res.render('common/pages/index');
     }
   });
 };
