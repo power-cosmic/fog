e62ecbe9f68b4bd2e7c12d9cd0e741be05d9e505
Tyler Hoffman <tyler.c.hoffman@gmail.com>
2015-12-05 11:08:02 -0500

    Admin token generation

--- app/controllers/admin.controller.js
+++ app/controllers/admin.controller.js
@@ -1,10 +1,79 @@
 var User = require('../models/users/user'),
   MongoClient = require('mongodb').MongoClient,
   ObjectId = require('mongodb').ObjectID,
-  database = require('../../config/config').db;
+  nodemailer = require('nodemailer'),
+  config = require('../../config/config'),
+  database = config.db,
+  parseUrl = require('url').parse,
+  emailCred = config.email,
+  transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: emailCred
+  }, {
+    from: emailCred.user,
+    headers: {}
+  }),
+  jasonStatham = transporter;
 
-exports.tokens = function(req, res) {
-  res.render('admin/pages/new-admin');
+  var jasonStatham = transporter;;
+
+exports.newToken = function(req, res) {
+  res.render('admin/pages/new-token');
+};
+
+exports.sendToken = function(req, res) {
+
+  var params = req.body,
+      emailAddress = params.email,
+      firstName = params.firstName,
+      lastName = params.lastName;
+
+  console.log(params)
+  console.log(emailAddress, firstName, lastName);
+
+  MongoClient.connect(database, function(err, db) {
+    if (err) {
+      console.log('error: ' + err);
+      res.json({
+        status: 'failure',
+        message: 'couldn\'t connect to database'
+      });
+    } else {
+      db.collection('users').insertOne({
+        email: emailAddress,
+        firstName: firstName,
+        lastName: lastName,
+        type: 'admin'
+      }, function (err, inserted) {
+        inserted = inserted.ops[0];
+        var id = inserted._id;
+        console.log('id: ' + id);
+        db.close();
+        if (err) {
+          res.json({
+            status: 'failure',
+            message: 'couldn\'t insert user into the database'
+          });
+        } else {
+
+          jasonStatham.sendMail({
+            from: emailCred.user,
+            to: emailAddress,
+            subject: 'Welcome to the fog family!',
+            html: '<p>Greetings from fog! Looks like you\'re a new hire. '
+                + 'To get set up as an admin, follow '
+                + '<a href="' + config.url + '/admin/register/' + id
+                + '">this link</a> to finalize your stuff!</p>'
+          });
+
+          res.json({
+            status: 'success'
+          });
+        }
+      });
+    }
+  });
+
 };
 
 exports.auth = function(req, res, next) {
--- app/routes/admin.routes.js
+++ app/routes/admin.routes.js
@@ -3,6 +3,9 @@ var admin = require('../controllers/admin.controller');
 module.exports = function(app) {
 
   app.route('/admin/new-token')
-    .get(admin.auth, admin.tokens);
+    .get(admin.auth, admin.newToken);
+
+  app.route('/admin/send-token')
+    .post(admin.auth, admin.sendToken);
 
 };
